c24e6e581335667a773ed664ece0256d95842559
Ronaldo Linhares <134345253+linharesx0F@users.noreply.github.com>
2025-03-13 14:37:51 -0300

    Update dumping.py

--- dumping.py
+++ dumping.py
@@ -3,7 +3,6 @@ import getpass
 import webbrowser, os
 
 def backup_mysql_db():
-    # Solicita informações do usuário
     host = input("Digite o host do banco de dados (ex: localhost): \n")
     user = input("Digite o usuário do banco de dados: \n")
     password = getpass.getpass("Digite a senha do banco de dados: \n")
@@ -15,9 +14,9 @@ def backup_mysql_db():
     dir_bin = None
     
     for dir_path in dir_mysql:
-        if os.path.exists(dir_path):  # Verifica se o caminho existe
-            dir_bin = dir_path  # Atribui o caminho completo do mysqldump.exe
-            break  # Sai do loop assim que encontrar um caminho válido
+        if os.path.exists(dir_path): 
+            dir_bin = dir_path  
+            break 
     
     if dir_bin:
         print(f"Caminho do mysqldump encontrado: {dir_bin}")
@@ -25,24 +24,19 @@ def backup_mysql_db():
         print("Caminho não encontrado! Por favor, verifique a instalação no disco local C.") 
     
     
-    # Pergunta se o usuário quer fazer o dump de todo o schema ou apenas de uma tabela
     dump_option = input("Você quer fazer o dump de todo o schema (S) ou apenas de uma tabela (T)? ").strip().upper()
     if dump_option == 'S':
-        # Backup de todo o schema
         backup_file = input("Digite o caminho completo do arquivo de backup (deixe em branco para usar o padrão): ").strip()
         if not backup_file:
-            # Caminho padrão
             default_backup_dir = "C:/dump" 
             if not os.path.exists(default_backup_dir):
                 os.makedirs(default_backup_dir)
             backup_file = os.path.join(default_backup_dir, f"{database}_full_backup.sql")    
         dump_exec = f"cd {dir_bin} && mysqldump -h {host} -u {user} -p{password} {database} > {backup_file}"
     elif dump_option == 'T':
-        # Backup de uma tabela específica
         table_name = input("Digite o nome da tabela: ")
         backup_file = input("Digite o caminho completo do arquivo de backup (deixe em branco para usar o padrão): ").strip()
         if not backup_file:
-            # Caminho padrão
             default_backup_dir = "C:/dump"
             if not os.path.exists(default_backup_dir):
                 os.makedirs(default_backup_dir)
@@ -54,7 +48,6 @@ def backup_mysql_db():
     
     
     
-    # Executa o comando de backup
     try:
         subprocess.run(dump_exec, shell=True, check=True)
         print(f"Backup realizado com sucesso no arquivo {backup_file}")
